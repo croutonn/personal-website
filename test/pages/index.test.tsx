77c676b50f751e45b4e8326f64aa7c7b8d55539c
croutonn <croutonna@gmail.com>
2020-11-15 22:56:26 +0900

    test: Fix home page test

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,17 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/jsx-pascal-case */
 import React from 'react'
 
-import { Home } from '@/pages/index'
+import * as HomePage from '@/pages'
 
 import { render, fireEvent } from '../test-utils'
 
-describe('Home page', () => {
+describe('Home page', async () => {
+  const { props: staticProps } = (await HomePage.getStaticProps({
+    params: { locale: 'ja' },
+  })) as { props: HomePage.HomePageProps; revalidate?: number | boolean }
+
   it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
+    const { asFragment } = render(<HomePage.default {...staticProps} />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 
   it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
+    const { getByText } = render(<HomePage.default {...staticProps} />, {})
     window.alert = jest.fn()
     fireEvent.click(getByText('Test Button'))
     expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
